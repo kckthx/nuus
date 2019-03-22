92dd13e592b63bdd320149cf4d92ce899c9a8cb3
N-Pex <npexdev@gmail.com>
2019-03-22 09:24:50 +0100

    Use cards for articles

--- src/components/ArticleList.vue
+++ src/components/ArticleList.vue
@@ -1,15 +1,26 @@
 <template>
-  <v-container>
-    <v-layout>
-        <a
-          v-for="(link, i) in articles"
-          :key="i"
-          :href="link.link"
-          class="subheading"
-          target="_blank"
-        >{{ link.title }}</a>
+<v-container>
+    <v-layout row wrap>
+      <v-list>
+        <v-card v-for="(item, i) in articles"
+          :key="i" class="ma-3"> 
+          <v-card-title primary-title>
+            <div>
+              <h3>{{ item.title }}</h3>
+            </div>
+          </v-card-title>
+          <v-card-text>
+              <div> {{ item.description }} </div>
+          </v-card-text>
+  
+          <v-card-actions>
+            <v-btn flat color="orange">Share</v-btn>
+            <v-btn flat color="orange">Explore</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-list>
     </v-layout>
-  </v-container>
+    </v-container>
 </template>
 
 
