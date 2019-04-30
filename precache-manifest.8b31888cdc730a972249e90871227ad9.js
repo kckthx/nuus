fa1243bce93694d0f0d5566dcef3facd2b36831f
N-Pex <npexdev@gmail.com>
2019-04-30 15:45:19 +0200

    deploy

---
+++ precache-manifest.8b31888cdc730a972249e90871227ad9.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/nuus/robots.txt"
+  },
+  {
+    "revision": "28b5e3203341f523a2ed",
+    "url": "/nuus/js/chunk-vendors.de2fa400.js"
+  },
+  {
+    "revision": "93f689551a5859b90552",
+    "url": "/nuus/js/app.9ff803a4.js"
+  },
+  {
+    "revision": "20c3e1c5625fa07cb18d",
+    "url": "/nuus/js/about.f25d08eb.js"
+  },
+  {
+    "revision": "9b74691616a08a6ce96e1950b72b6ffc",
+    "url": "/nuus/index.html"
+  },
+  {
+    "revision": "63a7d78d42c33b94fc7b957524795cac",
+    "url": "/nuus/img/logo.63a7d78d.svg"
+  },
+  {
+    "revision": "28b5e3203341f523a2ed",
+    "url": "/nuus/css/chunk-vendors.9596abeb.css"
+  },
+  {
+    "revision": "df8caa36bf37eca904a8296af7ed3703",
+    "url": "/nuus/assets/test.xml"
+  },
+  {
+    "revision": "05efaa032f3a32c0e2c391b3f879095a",
+    "url": "/nuus/assets/nasa.xml"
+  },
+  {
+    "revision": "fb16213e885f0e66102f3b00a5203eb0",
+    "url": "/nuus/assets/bundle.zip"
+  }
+];
